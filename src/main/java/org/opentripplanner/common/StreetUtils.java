8b549fcd54d185e36da5ebb8c75f05ea68bf0316
Vesa Meskanen <vesa.meskanen@cgi.com>
2017-05-26 12:12:51 +0300

    Take access limitations into account in island pruning
    Street access limitations can isolate part of street network from
    the rest of streets. Island pruning now detects such isolated areas.

--- src/main/java/org/opentripplanner/common/StreetUtils.java
+++ src/main/java/org/opentripplanner/common/StreetUtils.java
@@ -37,7 +37,15 @@ public class StreetUtils {
     private static Logger LOG = LoggerFactory.getLogger(StreetUtils.class);
     private static int islandCounter = 0;
 
-    public static void pruneFloatingIslands(Graph graph, int maxIslandSize, 
+    /* Island pruning strategy:
+       - First extract islands without considering noThruTraffic edges
+       - Then extract expanded islands by accepting noThruTraffic edges, but - DO NOT JUMP ACROSS ORIGINAL ISLANDS!
+         Note: these expanded islands can overlap.
+       - Generate also pure noThruTraffic islands
+       - Prune small islands away
+     */
+
+    public static void pruneFloatingIslands(Graph graph, int maxIslandSize,
             int islandWithStopMaxSize, String islandLogName, DataImportIssueStore issueStore) {
         LOG.debug("pruning");
         PrintWriter islandLog = null;
@@ -52,9 +60,65 @@ public class StreetUtils {
             islandLog.printf("%s\t%s\t%s\t%s\t%s\n","id","stopCount", "streetCount","wkt" ,"hadRemoved");
         }
         Map<Vertex, Subgraph> subgraphs = new HashMap<Vertex, Subgraph>();
+        Map<Vertex, Subgraph> extgraphs = new HashMap<Vertex, Subgraph>();
         Map<Vertex, ArrayList<Vertex>> neighborsForVertex = new HashMap<Vertex, ArrayList<Vertex>>();
 
-//        RoutingRequest options = new RoutingRequest(new TraverseModeSet(TraverseMode.WALK, TraverseMode.TRANSIT));
+        // Establish vertex neighbourhood without thruTrafficEdges
+        collectNeighbourVertices(graph, neighborsForVertex, false);
+
+        ArrayList<Subgraph> islands = new ArrayList<Subgraph>();
+        int graphCount;
+
+        /* associate each connected node with a subgraph */
+        graphCount = collectSubGraphs(graph, neighborsForVertex, subgraphs, null, null);
+        LOG.info("Islands without noThruTraffic: " + graphCount);
+
+        // Expand vertex neighbourhood with noThruTrafficEdges
+        // Note that we can reuse the original neighbour map here
+        // and simply process a smaller set of noThruTrafficEdges
+        collectNeighbourVertices(graph, neighborsForVertex, true);
+
+        /* Recompute expanded subgraphs by accepting noThruTraffic edges in graph expansion.
+           However, expansion is not allowed to jump from an original island to another one
+         */
+        graphCount = collectSubGraphs(graph, neighborsForVertex, extgraphs, subgraphs, islands);
+        LOG.info("Extended island count: " + graphCount);
+
+        /* Final round: generate noThruTraffic islands if such ones exist */
+        graphCount = collectSubGraphs(graph, neighborsForVertex, extgraphs, null, islands);
+        LOG.info("noThruTraffic island count: " + graphCount);
+
+        LOG.info("Total " + islands.size() + " sub graphs found");
+
+        /* remove all tiny subgraphs and large subgraphs without stops */
+        for (Subgraph island : islands) {
+            boolean hadRemoved = false;
+            if(island.stopSize() > 0){
+            //for islands with stops
+                if (island.streetSize() < islandWithStopMaxSize) {
+                    depedestrianizeOrRemove(graph, island, issueStore);
+                    hadRemoved = true;
+                }
+            }else{
+            //for islands without stops
+                if (island.streetSize() < maxIslandSize) {
+                    depedestrianizeOrRemove(graph, island, issueStore);
+                    hadRemoved = true;
+                }
+            }
+            if (islandLog != null) {
+                WriteNodesInSubGraph(island, islandLog, hadRemoved);
+            }
+        }
+        if (graph.removeEdgelessVertices() > 0) {
+            LOG.warn("Removed edgeless vertices after pruning islands");
+        }
+    }
+
+    private static void collectNeighbourVertices(
+        Graph graph, Map<Vertex, ArrayList<Vertex>> neighborsForVertex, boolean noThruTraffic) {
+
+        // RoutingRequest options = new RoutingRequest(new TraverseModeSet(TraverseMode.WALK, TraverseMode.TRANSIT));
         RoutingRequest options = new RoutingRequest(new TraverseModeSet(TraverseMode.WALK));
 
         for (Vertex gv : graph.getVertices()) {
@@ -68,6 +132,9 @@ public class StreetUtils {
                     e instanceof StreetTransitEntranceLink || e instanceof ElevatorEdge ||
                     e instanceof FreeEdge)
                 ) {
+                    continue;
+                }
+                if ((e instanceof StreetEdge && ((StreetEdge)e).isNoThruTraffic()) != noThruTraffic) {
                     continue;
                 }
                 State s1 = e.traverse(s0);
@@ -91,53 +158,45 @@ public class StreetUtils {
                 vertexList.add(in);
             }
         }
+    }
 
-        ArrayList<Subgraph> islands = new ArrayList<Subgraph>();
-        /* associate each node with a subgraph */
+    private static int collectSubGraphs(
+        Graph graph,
+        Map<Vertex, ArrayList<Vertex>> neighborsForVertex,
+        Map<Vertex, Subgraph> newgraphs, // put new subgraphs here
+        Map<Vertex, Subgraph> subgraphs, // optional isolation map from a previous round
+        ArrayList<Subgraph> islands) {   // final list of islands or null
+
+        int count=0;
         for (Vertex gv : graph.getVertices()) {
             if (!(gv instanceof StreetVertex)) {
                 continue;
             }
             Vertex vertex = gv;
-            if (subgraphs.containsKey(vertex)) {
+
+            if (subgraphs != null && !subgraphs.containsKey(vertex)) {
+                // do not start new graph generation from non-classified vertex
+                continue;
+            }
+            if (newgraphs.containsKey(vertex)) { // already processed
                 continue;
             }
             if (!neighborsForVertex.containsKey(vertex)) {
                 continue;
             }
-            Subgraph subgraph = computeConnectedSubgraph(neighborsForVertex, vertex);
+            Subgraph subgraph = computeConnectedSubgraph(neighborsForVertex, vertex, subgraphs);
             if (subgraph != null){
                 for (Iterator<Vertex> vIter = subgraph.streetIterator(); vIter.hasNext();) {
                     Vertex subnode = vIter.next();
-                    subgraphs.put(subnode, subgraph);
+                    newgraphs.put(subnode, subgraph);
                 }
-                islands.add(subgraph);
+                if (islands != null) {
+                    islands.add(subgraph);
+                }
+                count++;
             }
         }
-        LOG.info(islands.size() + " sub graphs found");
-        /* remove all tiny subgraphs and large subgraphs without stops */
-        for (Subgraph island : islands) {
-            boolean hadRemoved = false;
-            if(island.stopSize() > 0){
-            //for islands with stops
-                if (island.streetSize() < islandWithStopMaxSize) {
-                    depedestrianizeOrRemove(graph, island, issueStore);
-                    hadRemoved = true;
-                }
-            }else{
-            //for islands without stops
-                if (island.streetSize() < maxIslandSize) {
-                    depedestrianizeOrRemove(graph, island, issueStore);
-                    hadRemoved = true;
-                }
-            }
-            if (islandLog != null) {
-                WriteNodesInSubGraph(island, islandLog, hadRemoved);
-            }
-        }
-        if (graph.removeEdgelessVertices() > 0) {
-            LOG.info("Removed edgeless vertices after pruning islands");
-        }
+        return count;
     }
 
     private static void depedestrianizeOrRemove(
@@ -185,14 +244,26 @@ public class StreetUtils {
     }
 
     private static Subgraph computeConnectedSubgraph(
-            Map<Vertex, ArrayList<Vertex>> neighborsForVertex, Vertex startVertex) {
+        Map<Vertex, ArrayList<Vertex>> neighborsForVertex, Vertex startVertex, Map<Vertex, Subgraph> isolated) {
         Subgraph subgraph = new Subgraph();
         Queue<Vertex> q = new LinkedList<Vertex>();
+        Subgraph anchor = null;
+
+        if (isolated != null) {
+            // anchor subgraph expansion to this subgraph
+            anchor = isolated.get(startVertex);
+        }
         q.add(startVertex);
         while (!q.isEmpty()) {
             Vertex vertex = q.poll();
             for (Vertex neighbor : neighborsForVertex.get(vertex)) {
                 if (!subgraph.contains(neighbor)) {
+                    if (anchor != null) {
+                        Subgraph compare = isolated.get(neighbor);
+                        if ( compare != null && compare != anchor) { // do not enter a new island
+                            continue;
+                        }
+                    }
                     subgraph.addVertex(neighbor);
                     q.add(neighbor);
                 }
