34b9f5974b8666cf14ae6325dea20fc7a749dd44
Richárd Kőszegi <richard.koszegi@webvalto.hu>
2021-05-11 16:47:38 +0200

    Merge P+R and B+R OSM processing

--- src/main/java/org/opentripplanner/graph_builder/module/osm/OSMDatabase.java
+++ src/main/java/org/opentripplanner/graph_builder/module/osm/OSMDatabase.java
@@ -68,6 +68,9 @@ public class OSMDatabase {
     /* Map of all bike parking nodes, keyed by their OSM ID */
     private TLongObjectMap<OSMNode> bikeParkingNodes = new TLongObjectHashMap<>();
 
+    /* Map of all bike parking nodes, keyed by their OSM ID */
+    private TLongObjectMap<OSMNode> carParkingNodes = new TLongObjectHashMap<>();
+
     /* Map of all non-area ways keyed by their OSM ID */
     private TLongObjectMap<OSMWay> waysById = new TLongObjectHashMap<>();
 
@@ -163,6 +166,10 @@ public class OSMDatabase {
         return Collections.unmodifiableCollection(bikeParkingNodes.valueCollection());
     }
 
+    public Collection<OSMNode> getCarParkingNodes() {
+        return Collections.unmodifiableCollection(carParkingNodes.valueCollection());
+    }
+
     public Collection<Area> getWalkableAreas() {
         return Collections.unmodifiableCollection(walkableAreas);
     }
@@ -214,6 +221,9 @@ public class OSMDatabase {
         }
         if (node.isBikeParking()) {
             bikeParkingNodes.put(node.getId(), node);
+        }
+        if (node.isParkAndRide()) {
+            carParkingNodes.put(node.getId(), node);
         }
         if (!(waysNodeIds.contains(node.getId()) || areaNodeIds.contains(node.getId()) || node
                 .isStop())) {
