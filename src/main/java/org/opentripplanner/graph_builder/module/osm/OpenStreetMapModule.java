c68fd6cdc167a4ef68240c60b0660820f50da974
Zsombor Welker <zsombor.welker@realcity.io>
2021-05-14 19:14:32 +0200

    Make VehicleParking available from it's entrances

--- src/main/java/org/opentripplanner/graph_builder/module/osm/OpenStreetMapModule.java
+++ src/main/java/org/opentripplanner/graph_builder/module/osm/OpenStreetMapModule.java
@@ -63,6 +63,7 @@ import org.opentripplanner.routing.graph.Vertex;
 import org.opentripplanner.routing.services.notes.NoteMatcher;
 import org.opentripplanner.routing.util.ElevationUtils;
 import org.opentripplanner.routing.vehicle_parking.VehicleParking;
+import org.opentripplanner.routing.vehicle_parking.VehicleParking.VehicleParkingEntranceCreator;
 import org.opentripplanner.routing.vehicle_parking.VehicleParkingHelper;
 import org.opentripplanner.routing.vehicle_parking.VehicleParkingService;
 import org.opentripplanner.routing.vertextype.BarrierVertex;
@@ -391,14 +392,13 @@ public class OpenStreetMapModule implements GraphBuilderModule {
 
                 I18NString creativeName = nameParkAndRideEntity(isCarParkAndRide, node);
 
-                var entrance = VehicleParking.VehicleParkingEntrance.builder()
+                VehicleParkingEntranceCreator entrance = (builder) -> builder
                         .entranceId(new FeedScopedId(VEHICLE_PARKING_OSM_FEED_ID, String.format("%s/%s/entrance", node.getClass().getSimpleName(), node.getId())))
                         .name(creativeName)
                         .x(node.lon)
                         .y(node.lat)
                         .walkAccessible(true)
-                        .carAccessible(isCarParkAndRide)
-                        .build();
+                        .carAccessible(isCarParkAndRide);
 
                 var vehicleParking = createVehicleParkingObjectFromOsmEntity(
                         isCarParkAndRide, node.lon, node.lat, node, creativeName, List.of(entrance)
@@ -406,8 +406,8 @@ public class OpenStreetMapModule implements GraphBuilderModule {
 
                 vehicleParkingService.addVehicleParking(vehicleParking);
 
-                VehicleParkingEntranceVertex parkVertex = new VehicleParkingEntranceVertex(graph, entrance);
-                new VehicleParkingEdge(parkVertex, vehicleParking);
+                VehicleParkingEntranceVertex parkVertex = new VehicleParkingEntranceVertex(graph, vehicleParking.getEntrances().get(0));
+                new VehicleParkingEdge(parkVertex);
             }
 
             LOG.info("Created {} {} P+R nodes.", n, isCarParkAndRide ? "car" : "bike");
@@ -512,7 +512,7 @@ public class OpenStreetMapModule implements GraphBuilderModule {
                 }
             }
 
-            List<VehicleParking.VehicleParkingEntrance> entrances = createParkingEntrancesFromAccessVertices(accessVertices, creativeName.toString(), entity);
+            List<VehicleParking.VehicleParkingEntranceCreator> entrances = createParkingEntrancesFromAccessVertices(accessVertices, creativeName.toString(), entity);
 
             var vehicleParking = createVehicleParkingObjectFromOsmEntity(
                     isCarParkAndRide,
@@ -537,7 +537,7 @@ public class OpenStreetMapModule implements GraphBuilderModule {
                 double lat,
                 OSMWithTags entity,
                 I18NString creativeName,
-                List<VehicleParking.VehicleParkingEntrance> entrances
+                List<VehicleParking.VehicleParkingEntranceCreator> entrances
         ) {
             OptionalInt bicycleCapacity, carCapacity, wheelchairAccessibleCapacity;
             if (isCarParkAndRide) {
@@ -618,28 +618,25 @@ public class OpenStreetMapModule implements GraphBuilderModule {
             return accessVertices;
         }
 
-        private List<VehicleParking.VehicleParkingEntrance> createParkingEntrancesFromAccessVertices(
+        private List<VehicleParking.VehicleParkingEntranceCreator> createParkingEntrancesFromAccessVertices(
                 Set<OsmVertex> accessVertices,
                 String vehicleParkingName,
                 OSMWithTags entity
         ) {
-            List<VehicleParking.VehicleParkingEntrance> entrances = new ArrayList<>();
+            List<VehicleParking.VehicleParkingEntranceCreator> entrances = new ArrayList<>();
 
             for (var accessVertex : accessVertices) {
-                var entranceName = accessVertex.getName();
-                if (entranceName == null) {
-                    entranceName = String.format("#%d", entrances.size() + 1);
-                }
+                var vertexName = accessVertex.getName();
+                var entranceName = vertexName == null ? String.format("#%d", entrances.size() + 1) : vertexName;
 
-                entrances.add(VehicleParking.VehicleParkingEntrance.builder()
+                entrances.add((builder) -> builder
                     .entranceId(new FeedScopedId(VEHICLE_PARKING_OSM_FEED_ID, String.format("%s/%d/%d", entity.getClass().getSimpleName(), entity.getId(), accessVertex.nodeId)))
                     .name(new NonLocalizedString(String.format("%s (%s)", vehicleParkingName, entranceName)))
                     .x(accessVertex.getX())
                     .y(accessVertex.getY())
                     .vertex(accessVertex)
                     .walkAccessible(accessVertex.isConnectedToWalkingEdge())
-                    .carAccessible(accessVertex.isConnectedToDriveableEdge())
-                    .build());
+                    .carAccessible(accessVertex.isConnectedToDriveableEdge()));
             }
 
             return entrances;
