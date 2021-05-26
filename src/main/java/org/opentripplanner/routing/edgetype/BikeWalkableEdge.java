497532b70a8f3d2bae44fdde3ec903c3190a84b0
Zsombor Welker <zsombor.welker@realcity.io>
2021-05-26 09:25:17 +0200

    Add explicit bike / bike-walking / car / walk reluctance
    ... and speed parameters to RoutingRequest.
    
    Add explicit bike-switching to StreetEdge, along with a helper
    BikeWalkableEdge.

---
+++ src/main/java/org/opentripplanner/routing/edgetype/BikeWalkableEdge.java
@@ -0,0 +1,24 @@
+package org.opentripplanner.routing.edgetype;
+
+import org.opentripplanner.routing.api.request.RoutingRequest;
+import org.opentripplanner.routing.core.State;
+import org.opentripplanner.routing.core.StateEditor;
+import org.opentripplanner.routing.core.TraverseMode;
+import org.opentripplanner.routing.graph.Edge;
+
+public interface BikeWalkableEdge {
+
+    default void switchToWalkingBike(RoutingRequest options, StateEditor editor, boolean includeCostAndTime) {
+        editor.setBackWalkingBike(true);
+        if (includeCostAndTime) {
+            editor.incrementWeight(options.bikeSwitchCost);
+            editor.incrementTimeInSeconds(options.bikeSwitchTime);
+        }
+    }
+
+    default void switchToBiking(RoutingRequest options, StateEditor editor) {
+        editor.setBackWalkingBike(false);
+        editor.incrementWeight(options.bikeSwitchCost);
+        editor.incrementTimeInSeconds(options.bikeSwitchTime);
+    }
+}
