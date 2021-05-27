2269273deb4b166301838a97f0a06bbef2fcec52
Zsombor Welker <zsombor.welker@realcity.io>
2021-05-27 15:12:05 +0200

    Partially ignore and handle negative times when sorting triptimes

--- src/main/java/org/opentripplanner/routing/trippattern/TripTimes.java
+++ src/main/java/org/opentripplanner/routing/trippattern/TripTimes.java
@@ -268,7 +268,19 @@ public class TripTimes implements Serializable, Comparable<TripTimes>, Cloneable
      * trip down the line.
      */
     public int sortIndex() {
-        return getArrivalTime(0);
+        if (realTimeState == RealTimeState.CANCELED) {
+            return -1;
+        }
+
+        int arrivalTime = getArrivalTime(0);
+        if (arrivalTime >= 0) {
+            return arrivalTime;
+        }
+        int departureTime = getDepartureTime(0);
+        if (departureTime >= 0) {
+            return departureTime;
+        }
+        throw new IllegalStateException("Failed to sort trip, initial (stop = 0) arrival and departure is -1");
     }
 
     /** @return the time in seconds after midnight that the vehicle arrives at the stop. */
