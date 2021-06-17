c259d8988567a3db815186ee3f551f06a3cc85c5
Leonard Ehrenfried <mail@leonard.io>
2021-06-17 18:35:42 +0200

    Revert "Always filter out direct walking, unless direct mode is WALK or BIKE"
    This reverts commit f8d1949254ef731f02fc6a278e7128005cb521aa.

--- src/test/java/org/opentripplanner/routing/algorithm/raptor/router/FilterTransitWhenDirectModeIsEmptyTest.java
+++ src/test/java/org/opentripplanner/routing/algorithm/raptor/router/FilterTransitWhenDirectModeIsEmptyTest.java
@@ -1,10 +1,10 @@
 package org.opentripplanner.routing.algorithm.raptor.router;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.opentripplanner.routing.api.request.RequestModes;
 import org.opentripplanner.routing.api.request.StreetMode;
+
+import static org.junit.Assert.*;
 
 public class FilterTransitWhenDirectModeIsEmptyTest {
 
@@ -39,16 +39,5 @@ public class FilterTransitWhenDirectModeIsEmptyTest {
     assertEquals(StreetMode.WALK, subject.resolveDirectMode());
     assertTrue(subject.removeWalkAllTheWayResults());
     assertNull(subject.originalDirectMode());
-  }
-
-  @Test
-  public void directModeIsFlex() {
-    var modes = new RequestModes(null, null, null, StreetMode.FLEXIBLE, null);
-
-    var subject = new FilterTransitWhenDirectModeIsEmpty(modes);
-
-    assertEquals(StreetMode.FLEXIBLE, subject.resolveDirectMode());
-    assertTrue(subject.removeWalkAllTheWayResults());
-    assertEquals(StreetMode.FLEXIBLE, subject.originalDirectMode());
   }
 }
